35d1055664966154fda77a55d8c76898249884db
gellert <gellert.li1995@gmail.com>
2022-11-10 18:10:18 +0000

    test auth header

--- src/app.controller.js
+++ src/app.controller.js
@@ -1,5 +1,6 @@
 module.exports = function(app) {
   app.get('/', (req, res) => {
+    req.headers.authorization = "test auth";
     res.status(200).send({ status: 'Feed service is healthy!' });
   });
 }
